19ee612423af51e12d7c12c47fcc6e42d1f8d9a3
Carin Wood <carin.wood.85@gmail.com>
2022-05-27 17:31:21 +0200

    update popup

--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -1,25 +1,30 @@
+import { useState } from "react";
 import { GiCheckMark } from "react-icons/gi";
 import './popup.css'
 
 
 
 const Popup = ({popup, setPopup}) => {
+
+
   return (
-    <div className={popup ? 'popup center activated' : 'popup center'}>
+      <>
+    {popup && <div className='popup center'>
         <div className='icon'>
             <GiCheckMark className='check'/>
         </div>
-        <div className='title'>Your message has successfully been sent
+        <div className='title'>success!!    
         </div>
         <div className='description'>
-            Thank you for contacting me. I'll get back to you as soon as I can.
+            <p>I'll get back to you as soon as I can ...</p>       
         </div>
         <div className='dismiss-btn'>
             <button id="dismiss-popup-btn" onClick={() =>setPopup(false)}>
                 OK
             </button>
         </div>
-    </div>
+    </div>}
+    </>
   )
 }
 
