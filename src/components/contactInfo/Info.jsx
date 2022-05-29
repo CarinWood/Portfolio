b529b663eebff7a06e93ec8f7a0435b3fd76815d
Carin Wood <carin.wood.85@gmail.com>
2022-05-29 10:36:55 +0200

    update contact

--- src/components/contactInfo/Info.jsx
+++ src/components/contactInfo/Info.jsx
@@ -11,7 +11,7 @@ const Info = ({popup}) => {
       <div className="email-div">
         <MdAlternateEmail className="email-icon" />
         <article className="email-text">
-          <p className="email"> Get in touch with me</p>
+          <p className="_headline">Email</p>
           <p className="email-address">carin.wood.85@gmail.com</p>
         </article>
       </div>
@@ -19,25 +19,25 @@ const Info = ({popup}) => {
       <div className="location-div">
         <FiYoutube className="youtube-icon" />
         <article className="youtube-text">
-          <p className="location">Watch my videos</p>
-          <p className="youtube-subtext">To my Youtube</p>
+          <p className="_headline">Youtube</p>
+          <p className="youtube-subtext">Watch my videos</p>
         </article>
       </div>
     </section>
-    <section className="connect-section">
+    <section className={popup ? "connect-section dark" : "connect-section"}>
       <div className="email-div">
         <FiGithub className="email-icon" />
         <article className="email-text">
-          <p className="email">Check out my projects</p>
-          <p className="email-address">To my Github</p>
+          <p className="_headline">Github</p>
+          <p className="email-address">Check out my projects</p>
         </article>
       </div>
 
       <div className="location-div">
         <FiLinkedin className="linked-icon" />
         <article className="location-text">
-          <p className="location">Let's connect!</p>
-          <p className="linked-subtext">To my LinkedIn</p>
+          <p className="_headline">LinkedIn</p>
+          <p className="linked-subtext">Let's connect!</p>
         </article>
       </div>
     </section> 
